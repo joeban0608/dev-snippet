c2e8c5a55a382bef5f61e7589ff7e29ca6542310
joeban <joeanddaneil5@gmail.com>
2026-03-17 17:06:25 +0800

    fix: cast auth account expires_at during migration

--- drizzle/migrations/0001_marvelous_angel.sql
+++ drizzle/migrations/0001_marvelous_angel.sql
@@ -1,1 +1,3 @@
-ALTER TABLE "account" ALTER COLUMN "expires_at" SET DATA TYPE integer;
+ALTER TABLE "account"
+ALTER COLUMN "expires_at" SET DATA TYPE integer
+USING EXTRACT(EPOCH FROM "expires_at")::integer;
